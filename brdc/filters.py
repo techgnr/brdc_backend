bd745d14d602e59cca14d4cd62762e390590e9c1
Ganesh Kunwar <gnrtech744@gmail.com>
2025-07-20 12:16:03 +0545

    Refactor: added the filter

--- brdc/filters.py
+++ brdc/filters.py
@@ -3,8 +3,8 @@ from brdc.models import AboutSection
 
 
 class AboutSectionFilter(filters.FilterSet):
-    is_who_we_are = filters.BooleanFilter(field_name="is_curriculum")
-    is_what_we_do = filters.BooleanFilter(field_name="is_report")
+    is_who_we_are = filters.BooleanFilter(field_name="is_who_we_are")
+    is_what_we_do = filters.BooleanFilter(field_name="is_what_we_do")
     # created_at = filters.DateTimeFilter(field_name="created_at")
 
     class Meta:
