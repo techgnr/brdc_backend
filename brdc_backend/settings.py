6593d4c92849e16d2e45d92f6464744ba9a31f44
Ganesh Kunwar <gnrtech744@gmail.com>
2025-07-20 09:45:58 +0545

    Datatbase

--- brdc_backend/settings.py
+++ brdc_backend/settings.py
@@ -80,24 +80,24 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "HOST": "db",
-#         "NAME": "brdc_backend",
-#         "USER": "postgres",
-#         "PASSWORD": "postgres",
-#         "PORT": "5432",
-#     }
-# }
-
-# settings.py
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tripuras_brdc',
+        'USER': 'tripuras_brdc',
+        'PASSWORD': 'khadka@2082',
+        'HOST':'192.250.235.43',
+        'PORT':'3306',
     }
 }
+
+# # settings.py
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
